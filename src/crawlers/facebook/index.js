7f5b3415135db1706ab357f69ebac33303bb54f1
Jakub Kriz <ceskytunak@gmail.com>
2023-10-24 11:13:34 +0200

    feat: add crawler

--- src/crawlers/facebook/index.js
+++ src/crawlers/facebook/index.js
@@ -1,66 +1,64 @@
-import { Dataset, PlaywrightCrawler } from 'crawlee';
+import { Dataset, PlaywrightCrawler } from 'crawlee'
 
 // Create an instance of the PlaywrightCrawler class - a crawler
 // that automatically loads the URLs in headless Chrome / Playwright.
 const crawler = new PlaywrightCrawler({
-    launchContext: {
-        // Here you can set options that are passed to the playwright .launch() function.
-        launchOptions: {
-            headless: true,
-        },
-    },
+	launchContext: {
+		// Here you can set options that are passed to the playwright .launch() function.
+		launchOptions: {
+			headless: true,
+		},
+	},
 
-    // Stop crawling after several pages
-    maxRequestsPerCrawl: 50,
+	// Stop crawling after several pages
+	maxRequestsPerCrawl: 50,
 
-    // This function will be called for each URL to crawl.
-    // Here you can write the Playwright scripts you are familiar with,
-    // with the exception that browsers and pages are automatically managed by Crawlee.
-    // The function accepts a single parameter, which is an object with a lot of properties,
-    // the most important being:
-    // - request: an instance of the Request class with information such as URL and HTTP method
-    // - page: Playwright's Page object (see https://playwright.dev/docs/api/class-page)
-    async requestHandler({ request, page, enqueueLinks, log }) {
-        log.info(`Processing ${request.url}...`);
+	// This function will be called for each URL to crawl.
+	// Here you can write the Playwright scripts you are familiar with,
+	// with the exception that browsers and pages are automatically managed by Crawlee.
+	// The function accepts a single parameter, which is an object with a lot of properties,
+	// the most important being:
+	// - request: an instance of the Request class with information such as URL and HTTP method
+	// - page: Playwright's Page object (see https://playwright.dev/docs/api/class-page)
+	async requestHandler({ request, page, enqueueLinks, log }) {
+		log.info(`Processing ${request.url}...`)
 
-        // A function to be evaluated by Playwright within the browser context.
-        const data = await page.$$eval('article', ($posts) => {
-            const scrapedData: {
-                title: string;
-                href: string
-            }[] = [];
+		// A function to be evaluated by Playwright within the browser context.
+		const data = await page.$$eval('article', ($posts) => {
 
-            // We're getting the title, rank and URL of each post on Hacker News.
-            $posts.forEach(($post) => {
-                scrapedData.push({
-                    title: $post.querySelector('h2.entry-title a').innerText,
-                    href: $post.querySelector('h2.entry-title a').href
-                });
-            });
+			const scrapedData = []
 
-            return scrapedData;
-        });
+			// We're getting the title, rank and URL of each post on Hacker News.
+			$posts.forEach(($post) => {
+				scrapedData.push({
+					title: $post.querySelector('h2.entry-title a').innerText,
+					href: $post.querySelector('h2.entry-title a').href,
+				})
+			})
 
-        // Store the results to the default dataset.
-        await Dataset.pushData(data);
+			return scrapedData
+		})
 
-        // Find a link to the next page and enqueue it if it exists.
-        const infos = await enqueueLinks({
-            selector: 'h2.entry-title a',
-        });
+		// Store the results to the default dataset.
+		await Dataset.pushData(data)
 
-        if (infos.processedRequests.length === 0) log.info(`${request.url} is the last page!`);
-    },
+		// Find a link to the next page and enqueue it if it exists.
+		const infos = await enqueueLinks({
+			selector: 'h2.entry-title a',
+		})
 
-    // This function is called if the page processing failed more than maxRequestRetries+1 times.
-    failedRequestHandler({ request, log }) {
-        log.info(`Request ${request.url} failed too many times.`);
-    },
-});
+		if (infos.processedRequests.length === 0) log.info(`${request.url} is the last page!`)
+	},
 
-await crawler.addRequests(['https://ceskytunak.cz/']);
+	// This function is called if the page processing failed more than maxRequestRetries+1 times.
+	failedRequestHandler({ request, log }) {
+		log.info(`Request ${request.url} failed too many times.`)
+	},
+})
+
+await crawler.addRequests(['https://ceskytunak.cz/'])
 
 // Run the crawler and wait for it to finish.
-await crawler.run();
+await crawler.run()
 
-console.log('Crawler finished.');
+console.log('Crawler finished.')
---
+++ src/routes/crawlers.js
@@ -0,0 +1,22 @@
+import express from 'express'
+const router = express.Router()
+
+// middleware that is specific to this router
+router.use((req, res, next) => {
+	console.log('Time: ', Date.now())
+	next()
+})
+// define the home page route
+router.get('/', (req, res) => {
+	res.send('Crawlers home page')
+})
+// define the about route
+router.get('/about', (req, res) => {
+	res.send('About crwalers')
+})
+// define the facebook route
+router.get('/facebook', (req, res) => {
+	res.send('Facebook Crawler')
+})
+
+export default router
